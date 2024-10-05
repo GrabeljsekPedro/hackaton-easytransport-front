ebb763fec25adcff8c7464a0b8afd43f67e7ac74
GrabeljsekPedro <grabeljsekpedro@gmail.com>
2024-10-05 12:54:55 +0200

    init the app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <main>
+      <h1 className="text-4xl">Hello World</h1>
+      <p className="text-md">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+        voluptatibus eaque ab aperiam cumque fugit iste cupiditate asperiores
+        sit quibusdam totam porro saepe, tenetur laudantium nihil quisquam
+        officiis molestiae exercitationem.
+      </p>
+    </main>
+  );
+}
+
+export default App;
