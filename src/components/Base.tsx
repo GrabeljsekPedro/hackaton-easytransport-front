5b9e165d0430f6655c452011c254a86b9e30bb67
GrabeljsekPedro <grabeljsekpedro@gmail.com>
2024-10-06 01:15:13 +0200

    added map with route and duration

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -1,7 +1,11 @@
+import Map from "./Map";
+
 function Base() {
   return (
     <main className="h-[88%] grid grid-cols-2 gap-4 p-4">
-      <div className="bg-slate-500 rounded-md">Hola</div>
+      <div className="w-full">
+        <Map />
+      </div>
       <div className="bg-slate-500 rounded-md">Hola</div>
       <div className="col-span-2 bg-slate-500 rounded-md">Hola</div>
     </main>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 function Header() {
   return (
-    <header className="h-[12%] bg-slate-600 content-center">
-      <h1 className="text-4xl text-white">EasyTransport</h1>
+    <header className="h-[12%] content-center flex bg-[#0e66c7]">
+      <img className="ml-5 mr-20" src="./src/assets/logo.ico" />
+      <h1 className="text-5xl text-white content-center roboto-medium-italic">
+        EasyTransport
+      </h1>
     </header>
   );
 }
